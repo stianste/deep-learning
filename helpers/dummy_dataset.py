854d62ff4c038cac969881dfb82ad67f549201ea
stianste <stiansteinbakken94@gmail.com>
2018-11-12 16:37:09 +0100

    Let dummy data set repeat itself

--- helpers/dummy_dataset.py
+++ helpers/dummy_dataset.py
@@ -6,12 +6,24 @@ class DummyDataset(Dataset):
 
     def __init__(self):
         self.length = 32
+        self.repetitions = 16
         self.input_size = self.length // 2
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
+        input_tensor, tags, output_tensor = self.generate_pattern()
+        for i in range(self.repetitions - 1):
+            new_input_tensor, new_tags, new_output_tensor = \
+                self.generate_pattern()
+            input_tensor = torch.cat((input_tensor[:-1], new_input_tensor), 0)
+            tags = torch.cat((tags[:-1], new_tags), 0)
+            output_tensor = torch.cat((output_tensor[:-1], new_output_tensor), 0)
+
+        return input_tensor, tags, output_tensor
+
+    def generate_pattern(self):
         input_tensor = torch.zeros(self.length - 1, 1, self.input_size)
         output_tensor = torch.zeros(self.length - 1, 1, self.input_size)
         tags = torch.zeros(self.length - 1, 1, self.input_size)
@@ -27,3 +39,4 @@ class DummyDataset(Dataset):
                 self.input_size - i - 2] = 1
 
         return input_tensor, tags, output_tensor
+
